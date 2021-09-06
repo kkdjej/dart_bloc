e4c23583e5633f0ffbbc157d358e55e1eabd4c96
kwonkidae <kkdosk@naver.com>
2021-09-06 14:38:17 +0900

    add bloc

--- main.dart
+++ main.dart
@@ -1,29 +1,37 @@
 import 'package:bloc/bloc.dart';
 
 Stream<int> countStream(int max) async* {
-  for(int i = 0; i < max; i++) {
+  for (int i = 0; i < max; i++) {
     yield i;
   }
 }
 
-Future<int> sumStream(Stream<int> stream) async{
+Future<int> sumStream(Stream<int> stream) async {
   int sum = 0;
-  await for(int value in stream) {
+  await for (int value in stream) {
     sum += value;
   }
   return sum;
 }
 
 class CounterCubit extends Cubit<int> {
-  CounterCubit(): super(0);
+  CounterCubit() : super(0);
 
   void increment() {
+    addError(Exception("increment error!"), StackTrace.current);
     emit(state + 1);
   }
+
   @override
   void onChange(Change<int> change) {
     print(change);
     super.onChange(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+    super.onError(error, stackTrace);
   }
 }
 
@@ -33,6 +41,39 @@ class SimpleBlocObserver extends BlocObserver {
     super.onChange(bloc, change);
     print('${bloc.runtimeType} $change');
   }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace starckTrace) {
+    print('${bloc.runtimeType} $error $starckTrace');
+    super.onError(bloc, error, starckTrace);
+  }
+}
+
+enum CounterEvent { increment }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+    }
+  }
+
+  @override
+  void onChange(Change<int> change) {
+    print(change);
+    super.onChange(change);
+  }
+
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    super.onTransition(transition);
+    print(transition);
+  }
 }
 
 void main() async {
@@ -40,16 +81,28 @@ void main() async {
   // int sum = await sumStream(stream);
   // print(sum);
 
-  Bloc.observer = SimpleBlocObserver();
-  final cubit = CounterCubit();
+  // Bloc.observer = SimpleBlocObserver();
+  // final cubit = CounterCubit();
   // print(cubit.state);
   // cubit.increment();
   // print(cubit.state);
   // cubit.close();
-  final subscription = cubit.stream.listen(print);
-  cubit.increment();
+  // final subscription = cubit.stream.listen(print);
+  // cubit.increment();
+  // await Future.delayed(Duration.zero);
+  // await subscription.cancel();
+  // await cubit.close();
+
+  final bloc = CounterBloc();
+  // print(bloc.state);
+  // bloc.add(CounterEvent.increment);
+  // await Future.delayed(Duration.zero);
+  // print(bloc.state);
+  // await bloc.close();
+
+  final subscription = bloc.stream.listen(print);
+  bloc.add(CounterEvent.increment);
   await Future.delayed(Duration.zero);
   await subscription.cancel();
-  await cubit.close();
-
+  await bloc.close();
 }
